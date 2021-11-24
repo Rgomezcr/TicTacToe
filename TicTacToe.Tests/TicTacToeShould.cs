1b98879fb41af42fa423e23e94c6751e188f21aa
Raúl Gómez <rgomez@voxelgroup.net>
2021-11-24 10:35:31 +0100

    test: Is symbol x starting first

--- TicTacToe.Tests/TicTacToeShould.cs
+++ TicTacToe.Tests/TicTacToeShould.cs
@@ -1,14 +1,19 @@
 using System;
 using Xunit;
-
+using TicTacToe;
 namespace TicTacToe.Tests
 {
     public class TicTacToeShould
     {
         [Fact]
-        public void Test1()
+        public void IsSymbolXStartingFirst()
         {
+            TicTacToe ticTacToe = new TicTacToe();
+            string result = ticTacToe.InitialPlayer();
 
+            string expected = "X";
+
+            Assert.Equal(expected, result);
         }
     }
 }
